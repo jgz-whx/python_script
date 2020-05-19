533d154be246358025ff4944d150fa48ce4ff339
wanghexing <111>
2020-05-20 00:20:07 +0800

    更新文件

--- untitled.py
+++ untitled.py
@@ -12,11 +12,11 @@ with open(input_file,'r') as filereader:
         header_list = header.split(',')
         print (header_list)
         filewriter.write('#'.join(map(str,header_list))+'\n')
-#        for row in filereader :
-#            row = row.strip()
-#            row_list = row.split(',')
-#            print (row_list)
-#            filewriter.write(','.join(map(str,header_list))+'\n')
+       for row in filereader :
+           row = row.strip()
+           row_list = row.split(',')
+           print (row_list)
+           filewriter.write(','.join(map(str,header_list))+'\n')
 
 
 
