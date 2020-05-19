0b78866f68742d5923138dd3cd1b4ec4f0bfc74f
wanghexing <111>
2020-05-20 00:20:07 +0800

    新增解析Excel脚本

---
+++ excel_0520.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import sys
+from xlrd import open_workbook
+input_file = sys.argv[1]
+workbook = open_workbook(input_file)
+print('Number of worksheetd:',workbook.nsheets)
+for worksheet in workbook.sheets():
+    print("Worksheet name:",worksheet.name,"\tRows:",worksheet.nrows,"\tColumns:",worksheet.ncols)
+    print("Worksheet name:", worksheet.name)
+    print("\tRows:",worksheet.nrows)
+    print("\tColumns:",worksheet.ncols)
