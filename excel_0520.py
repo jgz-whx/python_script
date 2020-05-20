7ba3c2fb37a703eff23d2c2df1def7653802c5ff
wanghexing <111>
2020-05-21 00:52:11 +0800

    添加注释

--- excel_0520.py
+++ excel_0520.py
@@ -2,8 +2,11 @@
 import sys
 from xlrd import open_workbook
 input_file = sys.argv[1]
+# 加开Excel输入文件
 workbook = open_workbook(input_file)
-print('Number of worksheetd:',workbook.nsheets)
+# 打印出工作簿中工作表数量
+print("Number of worksheetd:",workbook.nsheets)
+# workbook.sheets ：识别出工作簿所有工作表
 for worksheet in workbook.sheets():
     print("Worksheet name:",worksheet.name,"\tRows:",worksheet.nrows,"\tColumns:",worksheet.ncols)
     print("Worksheet name:", worksheet.name)
